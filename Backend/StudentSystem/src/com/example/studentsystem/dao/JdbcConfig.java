6765afa4d14f18bd7aea85becc12ce2946314edd
Liu <xuyichenmo@gmail.com>
2023-06-01 20:27:25 +0800

    DB

--- Backend/StudentSystem/src/com/example/studentsystem/dao/JdbcConfig.java
+++ Backend/StudentSystem/src/com/example/studentsystem/dao/JdbcConfig.java
@@ -3,6 +3,6 @@ package com.example.studentsystem.dao;
 public interface JdbcConfig {
     String DRIVER = "com.mysql.jdbc.Driver";
     String URL = "jdbc:mysql://178.128.50.73:3306/StudentSystemDao";
-    String USERNAME = "root";
-    String PASSWORD = "";
+    String USERNAME = "StudentSystemDao";
+    String PASSWORD = "tfLLRkAzbNfAE4Dn";
 }
