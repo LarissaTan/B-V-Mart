43b0a6081285f7430ec4ae04fb47ab7f4c66a245
Liu <xuyichenmo@gmail.com>
2023-06-01 21:23:38 +0800

    client

---
+++ Server/StudentSystem/src/com/example/studentsystem/dao/JdbcConfig.java
@@ -0,0 +1,8 @@
+package com.example.studentsystem.dao;
+
+public interface JdbcConfig {
+    String DRIVER = "com.mysql.jdbc.Driver";
+    String URL = "jdbc:mysql://178.128.50.73:3306/StudentSystemDao";
+    String USERNAME = "StudentSystemDao";
+    String PASSWORD = "e8D3L6Ye8SiXMAXp";
+}
---
+++ Server/StudentSystem/src/com/example/studentsystem/run/TestLogin.java
@@ -0,0 +1,9 @@
+package com.example.studentsystem.run;
+
+import com.example.studentsystem.frame.StudentSystemLoginFrame;
+
+public class TestLogin {
+    public static void main(String[] args) {
+        StudentSystemLoginFrame frame = new StudentSystemLoginFrame();
+    }
+}
