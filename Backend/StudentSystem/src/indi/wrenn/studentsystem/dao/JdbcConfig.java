bc3c1a270c5778fa5d2227caaffda208c2f1e8e4
Liu <xuyichenmo@gmail.com>
2023-06-01 20:04:20 +0800

    rename

--- Backend/StudentSystem/src/indi/wrenn/studentsystem/dao/JdbcConfig.java
+++
@@ -1,8 +0,0 @@
-package indi.wrenn.studentsystem.dao;
-
-public interface JdbcConfig {
-    String DRIVER = "com.mysql.jdbc.Driver";
-    String URL = "jdbc:mysql://178.128.50.73:3306/StudentSystemDao";
-    String USERNAME = "root";
-    String PASSWORD = "";
-}
